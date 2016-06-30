c4025be1800993b8758c6b996f9b0ef488ee03e2
dsjoerg <dsjoerg@gmail.com>
2016-06-30 10:49:46 -0400

    five_hundred home controller for triggering exceptions to test airbrake

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   skip_before_action :require_complete_profile
   def sign_in
   end
+
+  def five_hundred
+    puts "YO YO YO"
+    puts 1/0
+    render text: "this is it"
+  end
 end
